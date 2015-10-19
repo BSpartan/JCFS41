0fb450f642330cd8a391fc0c6726984e6f38faa7
Bjork Verstraaten <bjork_verstraaten@hotmail.com>
2015-10-19 16:24:01 +0200

    Veranderingen collection project

--- Assignments/Collection koppeling/src/collection/koppeling/FXMLDocumentController.java
+++ Assignments/Collection koppeling/src/collection/koppeling/FXMLDocumentController.java
@@ -6,18 +6,23 @@
 package collection.koppeling;
 
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.util.Callback;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -40,48 +45,96 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     TextField addDepartment;
 
-    List<Employee> employees = Arrays.<Employee>asList(new Employee(
+    @FXML
+    AnchorPane mainPane;
+
+    final ObservableList<Employee> employees = FXCollections.observableArrayList(new Employee(
             "Jacob Smith", "Accounts Department"), new Employee("Isabella Johnson",
                     "Accounts Department"), new Employee("Mike Graham", "IT Support"),
             new Employee("Judy Mayer", "IT Support"), new Employee("Gregory Smith",
                     "IT Support"));
+    /*List<Employee> employees = Arrays.<Employee>asList(new Employee(
+     "Jacob Smith", "Accounts Department"), new Employee("Isabella Johnson",
+     "Accounts Department"), new Employee("Mike Graham", "IT Support"),
+     new Employee("Judy Mayer", "IT Support"), new Employee("Gregory Smith",
+     "IT Support"));*/
     TreeItem<String> rootNode;
-    
+
     TextFieldTreeCellImpl customField = new TextFieldTreeCellImpl();
 
     @FXML
     private void addNewPerson(ActionEvent event) {
-        employees.add(new Employee(addName.getText(), addDepartment.getText()));
+        addEmployeeToList(new Employee(addName.getText(), addDepartment.getText()));
+        for (Employee employee : employees) {
+             System.out.println(employee.getName());
+        }
+       
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        // TreeView
         this.rootNode = new TreeItem<>("MyCompany Human Resources");
-        
-        
+
         rootNode.setExpanded(true);
         for (Employee employee : employees) {
-            TreeItem<String> empLeaf = new TreeItem<>(employee.getName());
-            boolean found = false;
-            for (TreeItem<String> depNode : rootNode.getChildren()) {
-                if (depNode.getValue().contentEquals(employee.getDepartment())) {
-                    depNode.getChildren().add(empLeaf);
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                TreeItem<String> depNode = new TreeItem<>(employee.getDepartment());
-                rootNode.getChildren().add(depNode);
-                depNode.getChildren().add(empLeaf);
-            }
+            addEmployeeToList(employee);
         }
 
+        TreeView<String> treeView = new TreeView<>(rootNode);
+        treeView.setEditable(true);
+        treeView.setCellFactory((TreeView<String> p) -> new TextFieldTreeCellImpl());
+
+        mainPane.getChildren().add(treeView);
+
+        // TableView
+        mainTable.setEditable(true);
         
-        
-        mainTree.setRoot(rootNode);
-        mainTree.setEditable(true);
-        //mainTree.setCellFactory();
+        TableColumn nameCol = new TableColumn("Naam");
+        nameCol.setCellValueFactory(new PropertyValueFactory<Employee, String>("name"));
+        nameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        nameCol.setOnEditCommit(
+                new EventHandler<CellEditEvent<Employee, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Employee, String> t) {
+                        ((Employee) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())).setName(t.getNewValue());
+                    }
+                }
+        );
+
+        TableColumn departmentCol = new TableColumn("Afdeling");
+        departmentCol.setCellValueFactory(new PropertyValueFactory<Employee, String>("department"));
+        departmentCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        departmentCol.setOnEditCommit(
+                new EventHandler<CellEditEvent<Employee, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Employee, String> t) {
+                        ((Employee) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())).setDepartment(t.getNewValue());
+                    }
+                }
+        );
+
+        mainTable.getColumns().addAll(nameCol, departmentCol);
+        mainTable.setItems(employees);
     }
 
+    private void addEmployeeToList(Employee employee) {
+        TreeItem<String> empLeaf = new TreeItem<>(employee.getName());
+        boolean found = false;
+        for (TreeItem<String> depNode : rootNode.getChildren()) {
+            if (depNode.getValue().contentEquals(employee.getDepartment())) {
+                depNode.getChildren().add(empLeaf);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            TreeItem<String> depNode = new TreeItem<>(employee.getDepartment());
+            rootNode.getChildren().add(depNode);
+            depNode.getChildren().add(empLeaf);
+        }
+    }
 }
